d261743faeb16ce54064a613072cae7882baf521
samuel.behrmann@yahoo.de <samuel.behrmann@yahoo.de>
2024-05-16 13:32:31 +0200

    feat: setup project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_anwendungen/main.dart';
+import 'package:medi_support/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
