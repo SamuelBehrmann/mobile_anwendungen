7e469936b835df94659ac30413eb5288f18666b4
Jan <janaminger@googlemail.com>
2024-05-30 18:11:54 +0200

    customAppBar

---
+++ lib/ui/widgets/custom_app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+
+class CostumAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Option<String> title;
+  final Option<Widget> leading;
+  final List<Widget> actions;
+
+  const CostumAppBar({
+    super.key,
+    this.title = const None(),
+    this.leading = const None(),
+    this.actions = const <Widget>[],
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) => AppBar(
+        title: title
+            .map(
+              (String title) => Text(
+                title,
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                    ),
+              ),
+            )
+            .toNullable(),
+        centerTitle: true,
+        leading: leading.toNullable(),
+        actions: actions,
+      );
+}
