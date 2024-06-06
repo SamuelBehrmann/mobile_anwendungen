8d3c4febadac6ca6260e4711677a632924f2d90e
Luis Stumpf <luis.stumpf@medflex.de>
2024-06-06 10:43:50 +0200

    feat: integrate search into home

--- lib/ui/widgets/search.dart
+++ lib/ui/widgets/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medi_support/ui/widgets/icon_row.dart';
 
 class CustomSearchBar extends StatefulWidget {
   final List<String> items;
@@ -12,7 +13,7 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
   List<String> filterdList = <String>[];
 
   List<String> filterList(String text) =>
-      widget.items.where((element) => element.contains(text)).toList();
+      widget.items.where((String element) => element.contains(text)).toList();
 
   @override
   void initState() {
@@ -23,22 +24,41 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
 
   @override
   Widget build(BuildContext context) => SearchAnchor(
-        textCapitalization: TextCapitalization.words,
-        textInputAction: TextInputAction.search,
-        builder: (context, controller) => SearchBar(
-          hintText: "Search",
-          onTap: () => controller.openView(),
-          onChanged: (_) => controller.openView(),
+        builder: (BuildContext context, SearchController controller) =>
+            IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {
+            controller.openView();
+          },
         ),
-        suggestionsBuilder: (context, controller) =>
-            filterList(controller.text).map(
-          (e) => ListTile(
-            title: Text(e),
-            onTap: () {
-              controller.closeView(e);
-              controller.text = e;
-            },
-          ),
-        ),
+        suggestionsBuilder:
+            (BuildContext context, SearchController controller) {
+          Iterable<ListTile> list = filterList(controller.text).map(
+            (String e) => ListTile(
+              title: Text(e),
+              onTap: () {
+                controller
+                  ..closeView(e)
+                  ..text = e;
+              },
+            ),
+          );
+
+          return <Widget>[
+            ...list,
+            if (controller.text.isEmpty) ...<Widget>[
+              const Divider(),
+              const IconRow(
+                iconSize: 24.0,
+                icons: <IconWithLabel>[
+                  IconWithLabel(Icons.search, "test"),
+                  IconWithLabel(Icons.search, "test2"),
+                  IconWithLabel(Icons.search, "test3"),
+                  IconWithLabel(Icons.search, "test4"),
+                ],
+              ),
+            ],
+          ];
+        },
       );
 }
