86f425f104197446e64f81b411042b529ff069fb
samuel.behrmann@yahoo.de <samuel.behrmann@yahoo.de>
2024-06-06 09:07:34 +0200

    fix: main view

--- lib/ui/screens/main/main_view.dart
+++ lib/ui/screens/main/main_view.dart
@@ -30,43 +30,47 @@ class MainView extends ConsumerWidget {
     );
   }
 
-  Widget _buildContent() => SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            Message(
-              username: 'Name Surname',
-              userAvatar: Uri.parse(
-                'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-              ),
-              userTitles: const <String>['title1', 'title2'],
-              message:
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporLorem ipsum dolor sit amet, consectetur adipiscing elit, ',
-              replyCallback: (String messageId) {},
-            ),
-            Expanded(
-              child: ListView.separated(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-                itemBuilder: (_, int index) => PostPreview(
-                  title: 'Post Title',
-                  content:
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-                  account: PostPreviewAccount(
-                    name: 'Account Name',
-                    titles: <String>['Title 1', 'Title 2'],
-                    imageUrl: Uri.parse(
-                      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-                    ),
+  Widget _buildContent() => CustomScrollView(
+        slivers: <Widget>[
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate(<Widget>[
+                Message(
+                  username: 'Name Surname',
+                  userAvatar: Uri.parse(
+                    'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                   ),
-                  postId: 'test-post-id: $index',
+                  userTitles: const <String>['title1', 'title2'],
+                  message:
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporLorem ipsum dolor sit amet, consectetur adipiscing elit, ',
+                  replyCallback: (String messageId) {},
                 ),
-                separatorBuilder: (_, __) => const SizedBox(height: 16),
-                itemCount: 1000,
-              ),
+                const SizedBox(height: 24),
+              ]),
             ),
-          ],
-        ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            sliver: SliverList.separated(
+              itemBuilder: (_, int index) => PostPreview(
+                title: 'Post Title',
+                content:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+                account: PostPreviewAccount(
+                  name: 'Account Name',
+                  titles: <String>['Title 1', 'Title 2'],
+                  imageUrl: Uri.parse(
+                    'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=3276&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                  ),
+                ),
+                postId: 'test-post-id: $index',
+              ),
+              separatorBuilder: (_, __) => const SizedBox(height: 16),
+              itemCount: 1000,
+            ),
+          ),
+        ],
       );
 
   Widget _buildShuffleButton(
