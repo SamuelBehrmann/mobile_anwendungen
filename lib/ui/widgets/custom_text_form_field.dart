c8c86ac5c6d7976cf020cad8ec99a83de0c72de1
Jan <janaminger@googlemail.com>
2024-06-08 13:56:42 +0200

    make custom_text_form_field stateful
    stateful to track field input via controller

--- lib/ui/widgets/custom_text_form_field.dart
+++ lib/ui/widgets/custom_text_form_field.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 
-class CustomTextFormField extends StatelessWidget {
-  static const EdgeInsets _contentPadding = EdgeInsets.all(15);
-  static const EdgeInsets _paddingBetweenLabelField = EdgeInsets.only(top: 10);
-
-  final String labelText;
-  final String? hintText;
-  final IconData? icon;
+class CustomTextFormField extends StatefulWidget {
   final bool includeTextField;
+  final String labelText;
+  final String hintText;
+  final IconData? icon;
 
   const CustomTextFormField({
     super.key,
-    required this.labelText,
-    this.hintText,
-    this.icon,
     this.includeTextField = true,
+    required this.labelText,
+    required this.hintText,
+    this.icon,
   });
+
+  @override
+  CustomTextFormFieldState createState() => CustomTextFormFieldState();
+}
+
+class CustomTextFormFieldState extends State<CustomTextFormField> {
+  static const EdgeInsets _contentPadding = EdgeInsets.all(15);
+  static const EdgeInsets _paddingBetweenLabelField = EdgeInsets.only(top: 10);
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     List<Widget> widgets = <Widget>[];
 
-    if (includeTextField) {
+    if (widget.includeTextField) {
       widgets.add(
         Text(
-          labelText,
+          widget.labelText,
           style: Theme.of(context).textTheme.titleMedium,
         ),
       );
@@ -32,13 +50,14 @@ class CustomTextFormField extends StatelessWidget {
 
     widgets.add(
       Padding(
-        padding: includeTextField ? _paddingBetweenLabelField : EdgeInsets.zero,
+        padding: widget.includeTextField ? _paddingBetweenLabelField : EdgeInsets.zero,
         child: TextFormField(
+          controller: _controller,
           decoration: InputDecoration(
-            hintText: hintText,
-            border: const OutlineInputBorder(),
             contentPadding: _contentPadding,
-            prefixIcon: icon != null ? Icon(icon) : null,
+            hintText: widget.hintText,
+            border: const OutlineInputBorder(),
+            prefixIcon: widget.icon != null ? Icon(widget.icon) : null,
           ),
           maxLines: 1,
         ),
