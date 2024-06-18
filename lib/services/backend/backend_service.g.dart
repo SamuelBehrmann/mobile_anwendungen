3ef4c89efdf8ce25bef7c0fec88c8afea35469d7
Luis Stumpf <trumpf25lp@gmail.com>
2024-06-18 14:29:09 +0200

    feat: setup backend-service

---
+++ lib/services/backend/backend_service.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'backend_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$backendServiceAggregatorHash() =>
+    r'3848e2e87413957a44a9e825d048e17f4428c5ed';
+
+/// See also [backendServiceAggregator].
+@ProviderFor(backendServiceAggregator)
+final backendServiceAggregatorProvider =
+    Provider<BackendServiceAggregator>.internal(
+  backendServiceAggregator,
+  name: r'backendServiceAggregatorProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$backendServiceAggregatorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef BackendServiceAggregatorRef = ProviderRef<BackendServiceAggregator>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
